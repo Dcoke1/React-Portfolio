9253372db10ee570744566ef24b1b6361bf031c9
Douglas Coke <CEO@Douglass-MacBook-Pro.local>
2019-06-17 18:20:49 -0400

    Updates

---
+++ precache-manifest.a9352a64718421487e4a1a7ee93c5aee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66409315d454e17bdf8a68116585226",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "8d0f422320528b59ab1a",
+    "url": "/React-Portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "ec9ff01bcae8428f0a9f",
+    "url": "/React-Portfolio/static/css/main.27b3e902.chunk.css"
+  },
+  {
+    "revision": "8d0f422320528b59ab1a",
+    "url": "/React-Portfolio/static/js/2.c29790f5.chunk.js"
+  },
+  {
+    "revision": "ec9ff01bcae8428f0a9f",
+    "url": "/React-Portfolio/static/js/main.404200d9.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "b25cb3486a17650dd3a88ffac7a20440",
+    "url": "/React-Portfolio/static/media/logo.b25cb348.svg"
+  },
+  {
+    "revision": "fb7e8ef72dfb789e6ba148027f701431",
+    "url": "/React-Portfolio/static/media/mypic.fb7e8ef7.jpg"
+  },
+  {
+    "revision": "c3c3e762141518586d6d3ed4501aa2ea",
+    "url": "/React-Portfolio/static/media/web.c3c3e762.jpg"
+  }
+]);
