436bc71a3ae919cbb67d37026149776879ae96a6
Dcoke1 <Dcoke1@knights.ucf.edu>
2020-03-07 17:59:34 -0500

    remove mobile material icon

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.83995f7cf40afdfedba1083d2f93a055.js"
+  "/React-Portfolio/precache-manifest.bd1af9e6fd259c7b1e256d5101353a68.js"
 );
 
 self.addEventListener('message', (event) => {
