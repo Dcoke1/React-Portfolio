f898eb301e4dff0c00aed1e6536b086a29c920ff
Dcoke1 <Dcoke1@knights.ucf.edu>
2020-03-07 16:58:12 -0500

    Update 03/07/2020

---
+++ build/precache-manifest.e4c6d79fcd24ea040377f512f59f1e67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621fe080175ad81de3ab6759b6f98c9b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "c705a3092d45a508f8cb",
+    "url": "/React-Portfolio/static/css/2.ac220846.chunk.css"
+  },
+  {
+    "revision": "bc482c26bdbbc45d6133",
+    "url": "/React-Portfolio/static/css/main.cdc86601.chunk.css"
+  },
+  {
+    "revision": "c705a3092d45a508f8cb",
+    "url": "/React-Portfolio/static/js/2.735cfdbb.chunk.js"
+  },
+  {
+    "revision": "bc482c26bdbbc45d6133",
+    "url": "/React-Portfolio/static/js/main.d60ee35e.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "b25cb3486a17650dd3a88ffac7a20440",
+    "url": "/React-Portfolio/static/media/logo.b25cb348.svg"
+  },
+  {
+    "revision": "fb7e8ef72dfb789e6ba148027f701431",
+    "url": "/React-Portfolio/static/media/mypic.fb7e8ef7.jpg"
+  },
+  {
+    "revision": "c7cbbe770f0ad963da4665cec1031e9a",
+    "url": "/React-Portfolio/static/media/web-development-t.c7cbbe77.jpg"
+  },
+  {
+    "revision": "c3c3e762141518586d6d3ed4501aa2ea",
+    "url": "/React-Portfolio/static/media/web.c3c3e762.jpg"
+  }
+]);
